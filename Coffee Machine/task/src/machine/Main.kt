59bef32a41302c2a4184a3abcd2c46653b548647
JoJo <buyongjo@gmail.com>
2022-04-15 23:02:54 +0900

    Stage 2/6: Ingredient calculator

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,11 +1,44 @@
 package machine
 
 fun main() {
-    println("Starting to make a coffee")
-    println("Grinding coffee beans")
-    println("Boiling water")
-    println("Mixing boiled water with crushed coffee beans")
-    println("Pouring coffee into the cup")
-    println("Pouring some milk into the cup")
-    println("Coffee is ready!")
+    println("Write how many cups of coffee you will need:")
+    val count = readln().toInt()
+
+    val coffeeMenu = Menu(
+        mutableListOf(
+            Gradients(Type.WATER, 200, Unit.ML),
+            Gradients(Type.MILK, 50, Unit.ML),
+            Gradients(Type.COFFEE_BEANS, 15, Unit.G)
+        )
+    )
+
+    println("For $count cups of coffee you will need:")
+
+    val neededGradients = coffeeMenu.getNeededGradients(count)
+    for (gradient in neededGradients) {
+        println("${gradient.quantity} ${gradient.unit.value} of ${gradient.type.value}")
+    }
 }
+
+enum class Type(val value: String) {
+    WATER("water"),
+    MILK("milk"),
+    COFFEE_BEANS("coffee beans")
+}
+
+enum class Unit(val value: String) {
+    ML("ml"),
+    G("g")
+}
+
+class Gradients(val type: Type, val quantity: Int, val unit: Unit) {
+
+}
+
+class Menu(val gradients: List<Gradients>) {
+    fun getNeededGradients(count: Int): List<Gradients> {
+        return gradients.map { Gradients(it.type, it.quantity * count, it.unit) }
+    }
+}
+
+
