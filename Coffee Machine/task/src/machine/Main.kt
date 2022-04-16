68842e7d68dcbc2858a47a04323a6dd8995096d0
JoJo <buyongjo@gmail.com>
2022-04-16 23:31:52 +0900

    Stage 3/6: Estimate the number of servings (-ing)

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,29 +1,28 @@
 package machine
 
 fun main() {
+    val gradients = arrayListOf<Gradients>()
+    for (type in Type.values()) {
+        println("Write how many ${type.value} of ${type.unit} the coffee machine has:")
+        val quantity = readln().toInt()
+        gradients.add(Gradients(type, quantity))
+    }
+    val coffeeMenu = Menu(gradients)
+
     println("Write how many cups of coffee you will need:")
     val count = readln().toInt()
 
-    val coffeeMenu = Menu(
-        mutableListOf(
-            Gradients(Type.WATER, 200, Unit.ML),
-            Gradients(Type.MILK, 50, Unit.ML),
-            Gradients(Type.COFFEE_BEANS, 15, Unit.G)
-        )
-    )
-
-    println("For $count cups of coffee you will need:")
-
-    val neededGradients = coffeeMenu.getNeededGradients(count)
-    for (gradient in neededGradients) {
-        println("${gradient.quantity} ${gradient.unit.value} of ${gradient.type.value}")
+    if (coffeeMenu.hasEnoughGradients(count)) {
+        println("Yes, I can make that amount of coffee")
+    } else {
+        println("No, I can make only 0 cups of coffee")
     }
 }
 
-enum class Type(val value: String) {
-    WATER("water"),
-    MILK("milk"),
-    COFFEE_BEANS("coffee beans")
+enum class Type(val value: String, val unit: Unit) {
+    WATER("water", Unit.ML),
+    MILK("milk", Unit.ML),
+    COFFEE_BEANS("coffee beans", Unit.G)
 }
 
 enum class Unit(val value: String) {
@@ -31,14 +30,20 @@ enum class Unit(val value: String) {
     G("g")
 }
 
-class Gradients(val type: Type, val quantity: Int, val unit: Unit) {
+class Gradients(val type: Type, val quantity: Int) {
 
 }
 
 class Menu(val gradients: List<Gradients>) {
     fun getNeededGradients(count: Int): List<Gradients> {
-        return gradients.map { Gradients(it.type, it.quantity * count, it.unit) }
+        return gradients.map { Gradients(it.type, it.quantity * count) }
     }
+
+    fun hasEnoughGradients(count: Int): Boolean {
+        val neededGradients = getNeededGradients(count)
+        return neededGradients.all { gradient -> gradients.first { gradient.type == it.type }.quantity <= gradient.quantity }
+    }
+
 }
 
 
